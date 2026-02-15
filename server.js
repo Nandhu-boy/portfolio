a8a56408417184d1f81e47d2d71210fe8413dcec
nandhu-boy <nandhagopalv2005@gmail.com>
2026-02-15 23:30:53 +0530

    portfolio website

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+// static files
+app.use(express.static(path.join(__dirname, "public")));
+
+// route
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "views", "index.html"));
+});
+
+const PORT = 8000;
+
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
