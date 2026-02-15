3e1a7a507ff586ee9893a46e43bcb3d6c452c3fc
nandhu-boy <nandhagopalv2005@gmail.com>
2026-02-15 23:39:00 +0530

    updatedd

--- server.js
+++ server.js
@@ -3,14 +3,21 @@ const path = require("path");
 
 const app = express();
 
+// Serve static files
 app.use(express.static(path.join(__dirname, "public")));
 
+// Route for homepage
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "views", "index.html"));
 });
 
-const PORT = process.env.PORT || 8000;
+// Handle all routes (important for deployment)
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "views", "index.html"));
+});
+
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-    console.log("Server running");
+    console.log(`Server running on port ${PORT}`);
 });
